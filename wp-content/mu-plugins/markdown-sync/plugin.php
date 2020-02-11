e777da8a1c998fb455776fedcbfc62a50ee6014a
James Nylen <jnylen@gmail.com>
2020-02-11 04:23:21 +0000

    Add markdown-sync files
    From: https://github.com/WP-API/markdown-sync/commit/fcdb018e6b5582f17a12b0c6dbebcc18b8ca60ff

---
+++ wp-content/mu-plugins/markdown-sync/plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WPORG Markdown Importer
+ * Description: Automatic Markdown imports for handbooks and DevHub (CLI/API handbooks)
+ * Author: Daniel Bachhuber and Ryan McCue
+ */
+
+require __DIR__ . '/inc/class-yaml.php';
+require __DIR__ . '/inc/class-editor.php';
+require __DIR__ . '/inc/class-importer.php';
