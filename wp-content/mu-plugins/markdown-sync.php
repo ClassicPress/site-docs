5c8810d9c750cfbf01b412acb66fe2d3a655371b
James Nylen <jnylen@gmail.com>
2020-02-25 15:40:12 -0600

    Disable more editing features
    Since we're managing the content, title, and slug elsewhere, we should
    disable editing these fields in the dashboard.

--- wp-content/mu-plugins/markdown-sync.php
+++ wp-content/mu-plugins/markdown-sync.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once __DIR__ . '/markdown-sync/plugin.php';
+require_once __DIR__ . '/markdown-sync/class-cpdocs-editor.php';
 require_once __DIR__ . '/markdown-sync/class-cpdocs-importer.php';
 
 $cpnet_docs_importer = new CPDocs_Importer();
-$cpnet_docs_editor = new WordPressdotorg\Markdown\Editor( $cpnet_docs_importer );
+$cpnet_docs_editor = new CPDocs_Editor( $cpnet_docs_importer );
+add_action( 'init', [ $cpnet_docs_editor, 'init' ] );
 
 if ( class_exists( 'WP_CLI' ) ) {
 	WP_CLI::add_command(
